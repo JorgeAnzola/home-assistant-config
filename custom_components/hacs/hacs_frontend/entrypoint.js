fd9f373ec0855d214f62e6139bbb7f113610384c
Jorge Anzola <jorgeanzola@gmail.com>
2022-06-01 10:53:36 +0200

    🏠 Updated HACS components and fixed cameras 🎥

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,10 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-ff32767d.js')")();
+  new Function("import('/hacsfiles/frontend/main-c805434e.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-ff32767d.js';
+  el.src = '/hacsfiles/frontend/main-c805434e.js';
+  el.type = 'module';
   document.body.appendChild(el);
 }
   
